c23d6c5a6f4dabacd8c8402401105dff8fa95033
menteMaesta <paulagomenr@gmail.com>
2024-04-23 13:30:16 -0600

    Create tooling section

--- docs/.vuepress/components/Tooling.vue
+++ docs/.vuepress/components/Tooling.vue
@@ -1,16 +1,59 @@
 <template>
-  <div class="tooling">
-    {{ $site.locales[$localePath].tooling.map }}
-    <div></div>
+  <div class="toolingSection">
+    <p class="toolingTitle">{{ $site.locales[$localePath].tooling }}</p>
+    <div class="tools">
+      <div
+        class="tool"
+        v-for="(tool, index) in $site.locales[$localePath].tools"
+        :key="index"
+      >
+        <i v-if="tool.icon" :class="tool.icon" />
+        <img v-else :src="iconMap[tool.src]" alt="Tool Icon" />
+        {{ tool.name }}
+      </div>
+    </div>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import tailwindIcon from "@assets/tailwind-icon.svg";
+import adonisIcon from "@assets/adonis-icon.svg";
+import copilotIcon from "@assets/copilot-icon.svg";
+
+const iconMap = {
+  tailwind: tailwindIcon,
+  adonis: adonisIcon,
+  copilot: copilotIcon,
+};
+</script>
 
 <style scoped>
-.tooling {
+.toolingSection {
+  margin-top: 2rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.toolingTitle {
+  font-size: 1.2rem;
+  font-weight: bold;
+}
+.tools {
   display: flex;
   flex-direction: row;
-  align-items: center;
+  flex-wrap: wrap;
+  align-items: flex-start;
+  justify-content: space-evenly;
+}
+.tool {
+  margin-right: 1rem;
+  border: 1px solid black;
+  border-radius: 0.2rem;
+  padding: 0.2rem 0.5rem;
+  margin-bottom: 1rem;
+}
+img {
+  display: inline-block;
+  width: 1rem;
 }
 </style>
